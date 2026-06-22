b140d5466aabe721ad3d3c5f4972156955cc5ec0
jnzachos <95492232+jnzachos@users.noreply.github.com>
2026-06-22 23:06:42 +0300

    Fix comment

--- IntroToProgramming/programs/HelloWorld.c
+++ IntroToProgramming/programs/HelloWorld.c
@@ -2,7 +2,7 @@
                        This gives us printf(). */
 
 /* Every C program must have a main() function.
-   It is the entry point — where execution begins.
+   It is the entry point, where execution begins.
    It returns an int to tell the OS if it succeeded. */
 int main(void) {
 
