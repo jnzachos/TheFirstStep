6c38fa7ea074e5317fffbca1f0ad8306f9460242
jnzachos <95492232+jnzachos@users.noreply.github.com>
2026-07-22 17:24:47 +0300

    Add program

---
+++ IntroToProgramming/exercises_and_solutions/leetcode/TwoSum.c
@@ -0,0 +1,19 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* array = malloc(2*sizeof(int));
+
+    for (int i = 0; i < numsSize; i++){
+        for (int j = i+1; j < numsSize; j++){
+            if (nums[j] + nums[i] == target){
+                array[0] = i;
+                array[1] = j;
+                *returnSize = 2;
+                return array;
+            }
+        }
+    }
+    *returnSize = 0;
+    return array;
+}
